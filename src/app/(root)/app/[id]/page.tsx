386d56e899aca774cd1fe53a220e8c03da86f3af
Ayush Prasad <57259784+bulkpool@users.noreply.github.com>
2025-02-18 22:30:30 -0600

    build fix

--- src/app/(root)/app/[id]/page.tsx
+++ src/app/(root)/app/[id]/page.tsx
@@ -33,14 +33,14 @@ export default async function AppPage({
         <section className="relative flex flex-col grow w-full">
           <div className="flex w-full max-w-(--breakpoint-2xl) self-center items-center mb-4 px-4 md:px-16">
             <Image
-              src={app.icon}
-              alt={`${app?.name} icon`}
+              src={app.metadata.icon}
+              alt={`${app?.metadata.name} icon`}
               width={48}
               height={48}
               className="rounded-xl drop-shadow-md mr-4"
             />
             <h1 className="text-3xl lg:text-4xl font-extrabold tracking-tight">
-              {app?.name}
+              {app?.metadata.name}
             </h1>
           </div>
           <div className="w-full h-0.5 bg-neutral-100 rounded-full mb-0 md:mb-4" />
