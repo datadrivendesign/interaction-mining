eadfe28df5cdfd8ecb2e5586e8d11f727d623210
Erik Ji <erikjiv1.0@gmail.com>
2025-03-12 11:23:55 -0500

    working version

---
+++ src/app/study/new/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <div className="relative flex flex-col grow min-h-dvh items-center justify-start p-4 md:p-6">
+      <div className="flex flex-col w-full max-w-screen-2xl gap-4">
+        <h1 className="text-4xl font-extrabold tracking-tight">
+          Internal Study Management Tool
+        </h1>
+        <div></div>
+      </div>
+    </div>
+  );
+}
--- src/lib/actions/store-scraper.ts
+++ src/lib/actions/store-scraper.ts
@@ -2,12 +2,42 @@
 
 // @ts-ignore
 import appStore from "app-store-scraper";
+import { ok } from "assert";
 import gplay from "google-play-scraper";
 
 export async function getIosApp({ appId }: { appId: string }) {
-  return await appStore.app({ appId });
+  let res = await appStore.app({ appId });
+
+  if (!res) {
+    return {
+      ok: false,
+      message: "Failed to fetch app data",
+      data: null,
+    };
+  }
+
+  return {
+    ok: true,
+    message: "App data fetched",
+    data: res,
+  };
 }
 
 export async function getAndroidApp({ appId }: { appId: string }) {
-  return await gplay.app({ appId });
+
+  let res = await gplay.app({ appId });
+
+  if (!res) {
+    return {
+      ok: false,
+      message: "Failed to fetch app data",
+      data: null,
+    };
+  }
+
+  return {
+    ok: true,
+    message: "App data fetched",
+    data: res,
+  };
 }
