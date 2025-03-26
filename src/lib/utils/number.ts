00ea9362a479eecd3bddaf518479299f4b38fbe1
Erik Ji <erikjiv1.0@gmail.com>
2025-03-25 19:02:35 -0500

    WIP commit on capture flow with frame extraction/interaction snapshotting integrated

--- src/lib/utils/number.ts
+++ src/lib/utils/number.ts
@@ -2,9 +2,9 @@ import { format } from "d3-format";
 
 export function prettyNumber(
   number: number,
-  formatString: string = ".2s"
+  formatString: string = ".2f"
 ): string {
   // Create formatter using D3.js
   const formatter = format(formatString); // .2s means shortened suffix format with 2 significant digits
-  return formatter(number).toUpperCase(); // Format number and convert to uppercase
+  return formatter(number); // Format number and convert to uppercase
 }
