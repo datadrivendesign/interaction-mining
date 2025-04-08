c5763c31ebcc9e426dadb280f64d04f023da0f63
Carl Guo <carlguo2@illinois.edu>
2025-04-07 20:19:07 -0500

    memoize hooks to prevent unnecessary re-renders

--- src/app/trace/[traceId]/edit/components/repair-screen-canvas.tsx
+++ src/app/trace/[traceId]/edit/components/repair-screen-canvas.tsx
@@ -4,6 +4,7 @@ import React, {
   useCallback,
   useContext,
   useEffect,
+  useMemo,
   useState,
 } from "react";
 import Image from "next/image";
@@ -63,6 +64,10 @@ export default function RepairScreenCanvas({
 }) {
   const [imageRef, { width, height }] = useMeasure();
   const [mouse, ref] = useMouse();
+  const mergedRef = useMemo(
+    () => mergeRefs(ref, imageRef),
+    [ref, imageRef]
+  );
   const [tooltip, setTooltip] = useState<{
     x: number | null;
     y: number | null;
@@ -175,10 +180,7 @@ export default function RepairScreenCanvas({
               <figure className="relative w-full h-full">
                 <Image
                   ref={
-                    mergeRefs(
-                      ref,
-                      imageRef
-                    ) as React.MutableRefObject<HTMLImageElement | null>
+                    mergedRef as React.MutableRefObject<HTMLImageElement | null>
                   }
                   src={screen.src}
                   alt="gallery"
