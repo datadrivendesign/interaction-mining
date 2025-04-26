7742ee0a5b784850d2ce1e7134591a46f53d5bb1
Ayush Prasad <57259784+bulkpool@users.noreply.github.com>
2025-04-26 15:11:10 -0500

    Changed schema and added sign out and callbacks

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
+import {useSession} from "next-auth/react";
 
 import AppleProvider from "next-auth/providers/apple";
 import GoogleProvider from "next-auth/providers/google";
@@ -19,11 +20,48 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "select_account",
+        },
+      // authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code', 
+      },
+    
     }),
   ],
   pages: {
     signIn: "/sign-in",
+    signOut: "/sign-out",
   },
   debug: true,
   secret: process.env.NEXTAUTH_SECRET,
+
+  callbacks: {
+    async session({ session, token, }) {
+      if (session.user?.email) {
+        const dbUser = await prisma.user.findUnique({
+          where: { email: session.user.email },
+        });
+        if (dbUser) {
+          session.user.role = dbUser.role ?? "USER";
+        }
+      }
+      return session;
+    },
+  },
+  events: {
+    async signIn({ user, account, profile }) {
+      // Custom logic on sign in
+      console.log("User signed in:", user);
+    },
+    async signOut({ token }) {
+      // Custom logic on sign out
+      console.log("User signed out:", token);
+    },
+  },  
 });
+
+export const getSession = () => {
+  const { data: session, status } = useSession();
+  return { session, status };
+};
