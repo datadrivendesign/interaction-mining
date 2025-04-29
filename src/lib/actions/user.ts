7c8c63aa37cd5a75ec550954074d9abbe716b0b0
Ayush Prasad <57259784+bulkpool@users.noreply.github.com>
2025-04-29 18:38:18 -0500

    User dashboard logic

--- src/lib/actions/user.ts
+++ src/lib/actions/user.ts
@@ -1,81 +1,29 @@
 "use server";
 
 import { prisma } from "@/lib/prisma";
-import { App, Prisma } from "@prisma/client";
-// import { isObjectIdOrHexString } from "mongoose";
 
-interface GetAppParams {
-  limit?: number;
-  page?: number;
-  query?: string;
-  order?: "name" | "rating" | "downloads";
-  sort?: "asc" | "desc";
-}
-
-export async function getUser({
-  limit,
-  page = 1,
-  query = "",
-  order = "downloads",
-  sort = "desc",
-}: GetAppParams = {}) {
-  let app: App[] = [];
-
+export async function getUser(userId: string) {
   try {
-    const options: Prisma.AppFindManyArgs = {
-      where: {
-        metadata: {
-          is: {
-            name: {
-              contains: query,
-              mode: "insensitive",
-            },
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        image: true,
+        role: true,
+        traces: {
+          select: {
+            id: true,
+            createdAt: true,
           },
         },
       },
-      orderBy: {
-        metadata: {
-          [order]: sort,
-        },
-      },
-    } as Prisma.AppFindManyArgs;
+    });
 
-
-    // if limit is not provided, return all apps
-    if (limit) {
-      options.take = limit;
-    }
-
-    // if page and limit are provided, calculate the offset
-    if (page && limit) {
-      options.skip = (page - 1) * limit;
-    }
-
-    app = await prisma.app.findMany(options);
-  } catch (err: any) {
-    console.error(err);
-    throw new Error("Failed to fetch apps.");
-  }
-
-  return app;
-}
-
-export async function getApp(id: string): Promise<App | null> {
-  let app: App | null = {} as App;
-
-  if (!isObjectIdOrHexString(id)) {
+    return user;
+  } catch (error) {
+    console.error("Failed to fetch user details:", error);
     return null;
   }
-
-  try {
-    app = await prisma.app.findUnique({
-      where: {
-        id,
-      },
-    });
-  } catch {
-    throw new Error("Failed to fetch app.");
-  }
-
-  return app;
 }
