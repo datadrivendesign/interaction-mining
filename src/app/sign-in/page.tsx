4e57cec825f6c4fefc804f97d0dd79afd3367f06
Carl Guo <carlguo2@illinois.edu>
2025-05-02 05:54:10 -0500

    fix useSearchParams client side rendering need Suspense boundary

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -5,6 +5,7 @@ import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
 import { signIn, signOut } from "next-auth/react"; // use next-auth/react here
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function SignInPage() {
   const searchParams = useSearchParams();
@@ -33,60 +34,62 @@ export default function SignInPage() {
   };
 
   return (
-    <div className="flex w-dvw min-h-dvh justify-center items-start md:items-center p-8 md:p-16">
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle className="text-center text-2xl font-black">
-            Sign in to ODIM
-          </CardTitle>
+    <Suspense>
+      <div className="flex w-dvw min-h-dvh justify-center items-start md:items-center p-8 md:p-16">
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-center text-2xl font-black">
+              Sign in to ODIM
+            </CardTitle>
 
-          {/* Google */}
-          <Button
-            className="flex grow justify-center items-center w-full rounded-lg px-4 py-2"
-            onClick={handleGoogleSignIn}
-          >
-            <span className="inline-flex items-center text-white dark:text-black font-medium">
-              <Image
-                className="w-auto h-4 mr-4"
-                src="/third-party-logos/g.webp"
-                alt="Google logo"
-                width={0}
-                height={0}
-                sizes="100vw"
-              />
-              Sign in with Google
-            </span>
-          </Button>
+            {/* Google */}
+            <Button
+              className="flex grow justify-center items-center w-full rounded-lg px-4 py-2"
+              onClick={handleGoogleSignIn}
+            >
+              <span className="inline-flex items-center text-white dark:text-black font-medium">
+                <Image
+                  className="w-auto h-4 mr-4"
+                  src="/third-party-logos/g.webp"
+                  alt="Google logo"
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                />
+                Sign in with Google
+              </span>
+            </Button>
 
-          {/* Apple (disabled for now) */}
-          <Button
-            className="flex grow justify-center items-center w-full rounded-lg px-4 py-2"
-            disabled
-          >
-            <span className="inline-flex items-center text-white dark:text-black font-medium">
-              <Image
-                className="w-auto h-4 mr-4 invert dark:invert-0"
-                src="/third-party-logos/apple.svg"
-                alt="Apple logo"
-                width={0}
-                height={0}
-                sizes="100vw"
-              />
-              Sign in with Apple
-            </span>
-          </Button>
+            {/* Apple (disabled for now) */}
+            <Button
+              className="flex grow justify-center items-center w-full rounded-lg px-4 py-2"
+              disabled
+            >
+              <span className="inline-flex items-center text-white dark:text-black font-medium">
+                <Image
+                  className="w-auto h-4 mr-4 invert dark:invert-0"
+                  src="/third-party-logos/apple.svg"
+                  alt="Apple logo"
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                />
+                Sign in with Apple
+              </span>
+            </Button>
 
-          {/* Sign Out */}
-          {/* <Button
-            className="flex grow justify-center items-center w-full rounded-lg px-4 py-2"
-            onClick={handleSignOut}
-          >
-            <span className="inline-flex items-center text-white dark:text-black font-medium">
-              Sign out
-            </span>
-          </Button> */}
-        </CardHeader>
-      </Card>
-    </div>
+            {/* Sign Out */}
+            {/* <Button
+              className="flex grow justify-center items-center w-full rounded-lg px-4 py-2"
+              onClick={handleSignOut}
+            >
+              <span className="inline-flex items-center text-white dark:text-black font-medium">
+                Sign out
+              </span>
+            </Button> */}
+          </CardHeader>
+        </Card>
+      </div>
+    </Suspense>
   );
 }
