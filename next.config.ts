ca2247ca9adf08720cebd63942a0464467877ef3
Carl Guo <carlguo2@illinois.edu>
2025-05-28 03:13:03 -0500

    adjust next.config.ts to allow locally served images from minio. script improvements

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,39 @@
 import { NextConfig } from "next";
 import createMDX from "@next/mdx";
+import { RemotePattern } from "next/dist/shared/lib/image-config";
+
+const remotePatterns: RemotePattern[] = [
+  {
+    protocol: "https",
+    hostname: "play-lh.googleusercontent.com",
+  },
+  {
+    protocol: "https",
+    hostname: "*.amazonaws.com",
+  },
+  {
+    protocol: "https",
+    hostname: "*.mzstatic.com",
+  },
+];
+// Conditionally add MinIO endpoint if set
+if (process.env.MINIO_ENDPOINT) {
+  try {
+    const minioHost = new URL(process.env.MINIO_ENDPOINT).hostname;
+    remotePatterns.push({
+      protocol: "http",
+      hostname: minioHost,
+    });
+  } catch (err) {
+    console.warn("Invalid MINIO_ENDPOINT format in .env:", err);
+  }
+}
 
 const nextConfig: NextConfig = {
   turbopack: {},
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "play-lh.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.mzstatic.com",
-      },
-    ],
+    remotePatterns
   },
   experimental: {
     serverActions: {
