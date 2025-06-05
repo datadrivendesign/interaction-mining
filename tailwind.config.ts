aa69554cf8669e96bdef9c13395799d3ded718a0
Carl Guo <carlguo2@illinois.edu>
2025-06-04 18:07:58 -0700

    make code blocks look better in dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,7 +26,23 @@ const config: Config = {
         ],
         sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
         mono: ["var(--font-roboto-mono)", ...defaultTheme.fontFamily.mono],
-      },
+      },    
+      typography: ({ theme }) => ({
+        invert: {
+          css: {
+            pre: {
+              backgroundColor: theme('colors.zinc.800'), // lighter black than zinc.900
+              color: theme('colors.zinc.100'),
+            },
+            code: {
+              backgroundColor: theme('colors.zinc.800'),
+              color: theme('colors.zinc.100'),
+              padding: '0.2rem 0.4rem',
+              borderRadius: '0.25rem',
+            },
+          },
+        },
+      }),  
     },
   },
   plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
